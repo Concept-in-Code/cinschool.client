673b8b3703914e73355ba3db0d80db54cb64f16c
david feurle <david.feurle@liebherr.com>
2024-06-14 10:28:24 +0200

    test if the token is expired to determine login state

--- projects/common/api/src/services/common-auth.service.ts
+++ projects/common/api/src/services/common-auth.service.ts
@@ -4,6 +4,7 @@ import { ENVIRONMENT, Environment, Maybe, User } from 'common/core';
 import { Observable, tap } from 'rxjs';
 import { jwtToken } from '../constants/common-api.constants';
 import { CommonLoginUser } from '../typings/common-login-user';
+import { CommonLoginTokenPayload } from '../typings/common-login-token-payload';
 
 @Injectable({ providedIn: 'root'})
 export class CommonAuthService {
@@ -15,11 +16,18 @@ export class CommonAuthService {
   ) { }
 
   public get isAuthenticated(): boolean {
-    return !!this.token;
+    if(!this.tokenPaylod) return false;
+    return this.tokenPaylod.exp * 1000 > new Date().getTime()
   }
 
   public get token(): Maybe<string> {
     return localStorage.getItem(jwtToken);
+  }
+
+  private get tokenPaylod(): Maybe<CommonLoginTokenPayload> {
+    if(!this.token) return undefined
+
+    return JSON.parse(atob(this.token.split('.')[1]))
   }
 
   public login(user: CommonLoginUser): Observable<unknown> {
---
+++ projects/common/api/src/typings/common-login-token-payload.ts
@@ -0,0 +1,4 @@
+export type CommonLoginTokenPayload = {
+    sub: string,
+    exp: number,
+  }
